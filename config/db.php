3f20293b23a05c877a3ce2deea13c29370e64ec2
Yevgeniy <chernovzhenia@gmail.com>
2018-02-08 18:32:25 +0200

    change db file

--- config/db.php
+++ config/db.php
@@ -8,6 +8,9 @@ return [
 //    'dsn' => 'mysql:host=mysql.zzz.com.ua;dbname=eugene2509',
 //    'username' => 'eugene2509',
 //    'password' => 'Eugene2509.',
+//    'dsn' => 'mysql:host=127.0.0.1;dbname=dictionary',
+//    'username' => 'root',
+//    'password' => '',
     'charset' => 'utf8',
 
 
