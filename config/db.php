2c07c25b4276ea9354f9c56761a90a0eb88094bc
Yevgeniy <chernovzhenia@gmail.com>
2018-02-12 11:22:01 +0200

    change db.php

--- config/db.php
+++ config/db.php
@@ -2,12 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=dictionarydbinstance.cv4d4dcf5mb7.us-east-2.rds.amazonaws.com:3306;dbname=dictionary',
-    'username' => 'dictionary_user',
-    'password' => 'eugene2509',
-//    'dsn' => 'mysql:host=mysql.zzz.com.ua;dbname=eugene2509',
-//    'username' => 'eugene2509',
-//    'password' => 'Eugene2509.',
+    'dsn' => 'mysql:host=mysql.zzz.com.ua;dbname=eugene2509',
+    'username' => 'eugene2509',
+    'password' => 'Eugene2509.',
 //    'dsn' => 'mysql:host=127.0.0.1;dbname=dictionary',
 //    'username' => 'root',
 //    'password' => '',
